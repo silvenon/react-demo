50b42666ed192f2920f414136ee1fb2ff447cef1
Matija Marohnić <matija.marohnic@gmail.com>
2015-07-13 22:36:07 +0200

    Complete test case

--- test/switch.jsx
+++ test/switch.jsx
@@ -1,8 +1,6 @@
 import jsdomify from 'jsdomify';
 import assert from 'assert';
 
-var React, utils, Switch;
-
 describe.only('Switch', () => {
   before(() => {
     jsdomify.create();
@@ -19,57 +17,31 @@ describe.only('Switch', () => {
     jsdomify.clear();
   });
 
+  it('can use refs without crashing', () => {
+    const React = require('react/addons');
+    const utils = React.addons.TestUtils;
+    const Switch = require('../components/switch');
+
+    const el = utils.renderIntoDocument(
+      <Switch />
+    );
+
+    assert(el.refs.div);
+  });
+
+  it('can use refs without crashing', () => {
+    const React = require('react/addons');
+    const utils = React.addons.TestUtils;
+    const Switch = require('../components/switch');
+
+    const el = utils.renderIntoDocument(
+      <Switch />
+    );
+
+    assert(el.refs.div);
+  });
+
   after(() => {
     jsdomify.destroy();
-  });
-
-  it('defaults to off', () => {
-    React = require('react/addons');
-    utils = React.addons.TestUtils;
-    Switch = require('../components/switch');
-
-    const off = {el: <span>Off</span>, value: '0'};
-    const on = {el: <span>On</span>, value: '1'};
-    const el = utils.renderIntoDocument(
-      <Switch off={off} on={on} name="state" />
-    );
-
-    assert(!el.state.state);
-  });
-
-  it('renders props correctly', () => {
-    React = require('react/addons');
-    utils = React.addons.TestUtils;
-    Switch = require('../components/switch');
-
-    const off = {el: <span>Off</span>, value: '0'};
-    const on = {el: <span>On</span>, value: '1'};
-    const el = utils.renderIntoDocument(
-      <Switch off={off} on={on} name="state" state={true} />
-    );
-
-    assert.equal(React.findDOMNode(el.refs.value).textContent, 'On');
-    assert.equal(el.refs.select.props.name, 'state');
-    assert.equal(React.findDOMNode(el.refs.select).value, '1');
-  });
-
-  it('switches on click', () => {
-    React = require('react/addons');
-    utils = React.addons.TestUtils;
-    Switch = require('../components/switch');
-
-    const off = {el: <span>Off</span>, value: '0'};
-    const on = {el: <span>On</span>, value: '1'};
-    const el = utils.renderIntoDocument(
-      <Switch off={off} on={on} name="state" />
-    );
-    const value = React.findDOMNode(el.refs.value);
-    const select = React.findDOMNode(el.refs.select);
-
-    assert.equal(value.textContent, 'Off');
-    assert.equal(select.value, '0');
-    utils.Simulate.click(React.findDOMNode(el.refs.link));
-    assert.equal(value.textContent, 'On');
-    assert.equal(select.value, '1');
   });
 });
